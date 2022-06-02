afbf38f78bb7321355b4898b58078d06a650fed7
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-06-02 16:29:36 +0900

    register config

--- src/main/java/com/github/mikn/undying/UndyingEnchantmentMod.java
+++ src/main/java/com/github/mikn/undying/UndyingEnchantmentMod.java
@@ -1,9 +1,12 @@
 package com.github.mikn.undying;
 
+import com.github.mikn.undying.config.UndyingConfig;
 import com.github.mikn.undying.init.EnchantmentInit;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(UndyingEnchantmentMod.MODID)
@@ -11,6 +14,7 @@ public class UndyingEnchantmentMod {
     public static final String MODID = "undying";
     public UndyingEnchantmentMod() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, UndyingConfig.SPEC, "undying-common.toml");
         EnchantmentInit.ENCHANTMENTS.register(bus);
         MinecraftForge.EVENT_BUS.register(this);
     }
