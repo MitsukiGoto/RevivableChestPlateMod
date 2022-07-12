b5bc55e5514207105d83e426d72d591ad5f6fcb7
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2022-07-12 23:23:12 +0900

    add Enum Holder for Rarity

--- src/main/java/com/github/mikn/undying/config/UndyingConfig.java
+++ src/main/java/com/github/mikn/undying/config/UndyingConfig.java
@@ -4,6 +4,8 @@ import com.github.mikn.undying.UndyingEnchantmentMod;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.RequiresRestart;
+import net.minecraft.world.item.enchantment.Enchantment;
 
 @Config(name = UndyingEnchantmentMod.MODID)
 public class UndyingConfig implements ConfigData {
@@ -19,4 +21,9 @@ public class UndyingConfig implements ConfigData {
     }
     @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
     public UndyingCost undyingCost = UndyingCost.NORMAL;
+    @RequiresRestart
+    @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
+    public Enchantment.Rarity undyingRarity = Enchantment.Rarity.RARE;
+    @RequiresRestart
+    public boolean isTreasure = true;
 }
