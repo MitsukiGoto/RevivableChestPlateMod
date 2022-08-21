7778f9f92a2eb1d94e1ea34bc936dc322bd2cb78
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2022-08-21 15:13:16 +0900

    improve readability

--- src/main/java/com/github/mikn/undying/asm/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/mikn/undying/asm/mixin/LivingEntityMixin.java
@@ -22,40 +22,42 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-    @Inject(method= "checkTotemDeathProtection(Lnet/minecraft/world/damagesource/DamageSource;)Z", at=@At("RETURN"), cancellable = true)
+    @Inject(method = "checkTotemDeathProtection(Lnet/minecraft/world/damagesource/DamageSource;)Z", at = @At("RETURN"), cancellable = true)
     private void injection(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
         UndyingConfig config = AutoConfig.getConfigHolder(UndyingConfig.class).getConfig();
-        if(!cir.getReturnValue()) {
-            LivingEntity livingEntity = (LivingEntity) (Object) this;
-            int enchantmentLevel = EnchantmentHelper.getEnchantmentLevel(EnchantmentInit.UNDYING, livingEntity);
-            if(enchantmentLevel>0) {
-                if(livingEntity instanceof ServerPlayer serverPlayer) {
-                    Advancement advancementIn = serverPlayer.getServer().getAdvancements().getAdvancement(new ResourceLocation("adventure/totem_of_undying"));
-                    PlayerAdvancements playerAdvancements = serverPlayer.getAdvancements();
-                    boolean isPlayerUsedTotem = (advancementIn != null && playerAdvancements.getOrStartProgress(advancementIn).isDone());
-                    int playerExpLevel = serverPlayer.experienceLevel;
-                    int cost = isPlayerUsedTotem ? config.undyingCost.getInt() : (config.undyingCost.getInt())*2;
-                    if(playerExpLevel<cost) {
-                        return;
-                    }
-                    serverPlayer.giveExperienceLevels(-cost);
-                    Vec3 vec = new Vec3(serverPlayer.getX(), serverPlayer.getY(), serverPlayer.getZ());
-                    serverPlayer.connection.send(new ClientboundCustomSoundPacket(new ResourceLocation("item.totem.use"), serverPlayer.getSoundSource(), vec, 1.0f, 1.0f, serverPlayer.getRandom().nextLong()));
-                }
-                /*
-                * To avoid mobs other than players from being invincible, if the mobs has both absorption and fire_resistance,
-                * this enchantment will not affect them.
-                * */
-                if(!(livingEntity instanceof Player)) {
-                    if(livingEntity.hasEffect(MobEffects.ABSORPTION)&&livingEntity.hasEffect(MobEffects.FIRE_RESISTANCE)) return;
-                }
-                livingEntity.setHealth(3.0f);
-                livingEntity.removeAllEffects();
-                livingEntity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 900, 1));
-                livingEntity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 100, 1));
-                livingEntity.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 800, 0));
-                cir.setReturnValue(true);
+        if (cir.getReturnValue()) return;
+        LivingEntity livingEntity = (LivingEntity) (Object) this;
+        int enchantmentLevel = EnchantmentHelper.getEnchantmentLevel(EnchantmentInit.UNDYING, livingEntity);
+        if (enchantmentLevel <= 0) return;
+        if (livingEntity instanceof ServerPlayer serverPlayer) {
+            Advancement advancementIn = serverPlayer.getServer().getAdvancements()
+                    .getAdvancement(new ResourceLocation("adventure/totem_of_undying"));
+            PlayerAdvancements playerAdvancements = serverPlayer.getAdvancements();
+            boolean isPlayerUsedTotem = (advancementIn != null && playerAdvancements.getOrStartProgress(advancementIn).isDone());
+            int playerExpLevel = serverPlayer.experienceLevel;
+            int cost = isPlayerUsedTotem ? config.undyingCost.getInt() : (config.undyingCost.getInt()) * 2;
+            if (playerExpLevel < cost) {
+                return;
             }
+            serverPlayer.giveExperienceLevels(-cost);
+            Vec3 vec = new Vec3(serverPlayer.getX(), serverPlayer.getY(), serverPlayer.getZ());
+            serverPlayer.connection.send(new ClientboundCustomSoundPacket(new ResourceLocation("item.totem.use"),
+                    serverPlayer.getSoundSource(), vec, 1.0f, 1.0f, serverPlayer.getRandom().nextLong()));
         }
+        /*
+         * To avoid mobs other than players from being invincible, if the mobs has both
+         * absorption and fire_resistance,
+         * this enchantment will not affect them.
+         */
+        if (!(livingEntity instanceof Player)) {
+            if (livingEntity.hasEffect(MobEffects.ABSORPTION) && livingEntity.hasEffect(MobEffects.FIRE_RESISTANCE))
+                return;
+        }
+        livingEntity.setHealth(3.0f);
+        livingEntity.removeAllEffects();
+        livingEntity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 900, 1));
+        livingEntity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 100, 1));
+        livingEntity.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 800, 0));
+        cir.setReturnValue(true);
     }
 }
